facb18fd4fc27d8f77bdbba73bf4d0b94796ad6c
Volodymyr Kovaliak <vovakovaliak@gmail.com>
2026-06-09 17:10:42 +0300

    validation for modals

--- FlowBoard.Frontend.Domain/Models/Boards/CreateBoardModel.cs
+++ FlowBoard.Frontend.Domain/Models/Boards/CreateBoardModel.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FlowBoard.Frontend.Domain.Models.Boards;
 
 public class CreateBoardModel
 {
+    [Required(ErrorMessage = "Board name is required.")]
+    [StringLength(100, ErrorMessage = "Board name must not be more than 100 characters.")]
     public string Name { get; set; } = string.Empty;
     public bool IsPublic { get; set; } = true;
 }
