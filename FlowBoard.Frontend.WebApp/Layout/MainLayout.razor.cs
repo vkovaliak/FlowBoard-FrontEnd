7f3f858ded7accff1fb5becc3f5f552bf1c461b8
Volodymyr Kovaliak <vovakovaliak@gmail.com>
2026-06-19 16:16:00 +0300

    added base design

--- FlowBoard.Frontend.WebApp/Layout/MainLayout.razor.cs
+++ FlowBoard.Frontend.WebApp/Layout/MainLayout.razor.cs
@@ -1,20 +1,27 @@
 using FlowBoard.Frontend.Services.Abstractions;
 using Microsoft.AspNetCore.Components;
+using FlowBoard.Frontend.WebApp.Theme;
+using MudBlazor;
 
 namespace FlowBoard.Frontend.WebApp.Layout;
 
 public partial class MainLayout
 {
-    [Inject] 
-    public IAuthService AuthService { get; set; } = default!;
+    [Inject] public IAuthService AuthService { get; set; } = default!;
+    [Inject] public NavigationManager NavigationManager { get; set; } = default!;
 
-    [Inject]
-    public NavigationManager NavigationManager { get; set; } = default!;
-    
+    private MudTheme _theme = AppTheme.Build();
+    private bool _isDarkMode = false;
+    private bool _drawerOpen = true;
+
+    private void ToggleDrawer()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+
     private async Task HandleLogoutAsync()
     {
         await AuthService.LogoutAsync();
-
         NavigationManager.NavigateTo("/login");
     }
 }
